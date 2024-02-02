000de7dee19742ca3350022a113cd0768a9ef062
terminalapp <115147556+terminalapp@users.noreply.github.com>
2024-02-02 12:14:05 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const fetchData = (url, setIsLoading, setIsTargetUser) => {
-    axios.get(url)
+    axios.get("https://mind-breaker-apps.com/filter.php")
         .then(response => {
             setIsLoading(false);
             if (response.status === 200) {
@@ -11,9 +11,8 @@ const fetchData = (url, setIsLoading, setIsTargetUser) => {
             }
         })
         .catch(error => {
-            console.log('Произошла ошибка:', error);
             setIsLoading(false);
         });
-};
+}, []);
 
 export default fetchData;
